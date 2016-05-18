50b0c6a69abcd8a15b8d1ffd66849600525d9b23
Bembeng Arifin <bembengarifin@gmail.com>
2016-05-19 03:35:32 +0800

    fix problem 2

--- Problem2.py
+++ Problem2.py
@@ -1,19 +1,15 @@
-sum = 0
 
-#a, b = 0, 1
-#while a < 4000000:
-#    print a
-#    sum += a
-#    c = a + b
-#    a = b + c
-#    b = c + a
 
-a, b = 0, 1
-while a < 4000000:
-    print a
-    c = a 
-    a += b 
-    b = c 
+def run():
+    aggr = 0
+    first, second = 0, 1
+    while first < 4000000:
+        # print a
+        if first % 2:
+            aggr = aggr + first
+        temp = first
+        first = first + second
+        second = temp
+    print aggr
 
-print sum
-    
+run()
