0ed2cea2ff390eb28ffe60d624106a772d1070ce
Bembeng Arifin <bembengarifin@gmail.com>
2016-05-19 03:21:33 +0800

    fixed problem 1

--- Problem1.py
+++ Problem1.py
@@ -1,21 +1,14 @@
-#print reduce ( lambda x, y : x + y, [x for x in range(100000000) if x % 3 == 0 or x % 5 == 0])
-
-#sum = 0
-#for x in xrange(3, 1000000000):
-#    if x % 3 == 0 or x % 5 == 0:
-#        sum += x
-#print sum
 
 
-sum = 0
-index = 3
-while index < 1000000000:
-    if index % 3 == 0 or index % 5 == 0:
-        sum += index
-    index += 1
-print sum
-#
-#print index
+def run():
+    aggr = 0
+    index = 3
+    while index < 1000:
+        if index % 3 == 0 or index % 5 == 0:
+            aggr += index
+        index += 1
+    print aggr
 
+print reduce(lambda x, y: x + y, (x for x in xrange(1000) if x % 3 == 0 or x % 5 == 0))
 
-#print reduce ( lambda x, y : x + y, (x for x in xrange(1000000000) if x % 3 == 0 or x % 5 == 0))
+run()
