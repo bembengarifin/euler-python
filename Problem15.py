62c47373d6a6b603e179eb9c40b0d965ff1912d6
Bembeng Arifin <bembengarifin@gmail.com>
2016-06-01 09:11:29 +0800

    another attempt at 15

--- Problem15.py
+++ Problem15.py
@@ -1,3 +1,4 @@
+import datetime
 
 
 def move():
@@ -5,14 +6,14 @@ def move():
     simulate the movement from Y to X
     '''
 
-    
+
     turning_points = []
-    routes = []
-    size_x = 3
-    size_y = 3
+    #routes = []
+    routes = 0
+    size_x = 20
+    size_y = 20
 
     while True:
-
         route = []
         last_position = (0, 0)
 
@@ -25,9 +26,9 @@ def move():
                     last_position = (move_y, move_x)
                     route.append(last_position)
                     #print last_position
-                    if previous_position[1] != last_position[1] and last_position != (size_y, size_x):
+                    if previous_position[1] != last_position[1] and last_position[0] != size_y:
                         last_turning_point = last_position
-                        #print 'Turning Point:' + str(last_turning_point)
+                        #print last_turning_point
 
                 move_x += 1
                 if move_x > size_x or (move_y, move_x) in turning_points:
@@ -37,19 +38,35 @@ def move():
             if move_y > size_y:
                 break
 
-        print route
+        ' if prev route turning point x index is different than current last turning point then set the turning points to last turning point only + previous y,1 indexes'
+        if len(turning_points) > 1 and turning_points[-1][1] != last_turning_point[1]:
+            temp_points = []
+            for point in turning_points:
+                if point[0] < last_turning_point[0] or point[1] < last_turning_point[1]:
+                    temp_points.append(point)
+            turning_points = temp_points
 
-        # ' clear the turning points for new Y route calculation
-        if last_turning_point == (0, 1):
-            turning_points = []
         turning_points.append(last_turning_point)
-        routes.append(route)
 
-        # ' if the last turning point is the same as the height, means this is the last route
-        if last_turning_point[0] == size_y:
+        #print route
+        #print len(turning_points)
+
+        #routes.append(route)
+        routes += 1
+
+        # ' if the route contains the bottom left (size y, 0), means that's the last route
+        if (size_y, 0) in route:
             break
 
-    print len(routes)
+    #print len(routes)
+    print routes
 
+
+start_time = datetime.datetime.now()
 
 move()
+
+end_time = datetime.datetime.now()
+
+duration = end_time - start_time
+print duration
